ffeb1f3295ef7acb16990ae820b778806f1ac0cd
shameela2004 <fathimashameela2004@gmail.com>
2025-10-17 16:57:36 +0530

    new

--- ToDoApplication/src/app/app.ts
+++ ToDoApplication/src/app/app.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
